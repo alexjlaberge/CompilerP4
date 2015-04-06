ee5e6a15e7997c95404a20dadb7b5466f4306770
Alec Ten Harmsel <talec@eecs2331p03.engin.umich.edu>
2015-04-05 20:17:21 -0400

    Update tacdiff.sh to run all tests if no arg given

--- tacdiff.sh
+++ tacdiff.sh
@@ -4,8 +4,18 @@ CURRENT_OUTPUT="/tmp/eecs483.out"
 
 make
 
-echo "Solution | Ours"
+if [ ! -z ${1} ]
+then
+	echo "Solution | Ours"
 
-./dcc -d tac < samples/${1}.decaf > ${CURRENT_OUTPUT}
-./solution/dcc -d tac < samples/${1}.decaf | \
-        diff -aw - ${CURRENT_OUTPUT}
+	./dcc -d tac < samples/${1}.decaf > ${CURRENT_OUTPUT}
+	./solution/dcc -d tac < samples/${1}.decaf | diff -aw - ${CURRENT_OUTPUT}
+	exit $?
+fi
+
+for f in `ls samples/*.decaf`
+do
+	echo -n "Testing ${f}..."
+	./dcc -d tac < ${f} 2> /dev/null > ${CURRENT_OUTPUT}
+	./solution/dcc -d tac < ${f} 2> /dev/null | diff -awq - ${CURRENT_OUTPUT} && echo "PASS"
+done
